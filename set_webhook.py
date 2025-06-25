a0984a903f5d188596879742cae2baa8677eae5f
VALOKING <115923089+VALOKING@users.noreply.github.com>
2025-06-25 11:10:46 +0530

    Update set_webhook.py

--- set_webhook.py
+++ set_webhook.py
@@ -1,7 +1,7 @@
 import requests
 
 BOT_TOKEN = "8120217348:AAFo7KKaRXPdL-uh43J2sFIP6Ook4bWkHug"
-WEBHOOK_URL = "https://github.com/VALOKING/rcservice-bot"
+WEBHOOK_URL = "https://rcservice-bot.onrender.com"
 
 response = requests.get(
     f"https://api.telegram.org/bot{BOT_TOKEN}/setWebhook?url={WEBHOOK_URL}"
