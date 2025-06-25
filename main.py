55ae09a1f9a2a655851d47581dee506e8a960a55
VALOKING <115923089+VALOKING@users.noreply.github.com>
2025-06-25 11:45:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -32,6 +32,8 @@ def webhook():
 
     return {"ok": True}
 
+import os
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=10000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
